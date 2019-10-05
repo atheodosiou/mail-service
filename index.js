ff6528a12509b2baaff129f448174642c80465e7
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-10-05 21:48:25 +0300

    changes

--- index.js
+++ index.js
@@ -4,23 +4,9 @@ const app = express();
 const dotenv = require("dotenv");
 let port = 3000;
 const bodyParser = require("body-parser");
-// import bodyParser from 'body-parser';
 
 dotenv.config();
 port = process.env.PORT || 3000;
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   provider: "gmail",
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: "anastasios.theodosiou@gmail.com", // Enter here email address from which you want to send emails
-//     pass: "theodosiou91alex" // Enter here password for email account from which you want to send emails
-//   },
-//   tls: {
-//     rejectUnauthorized: false
-//   }
-// });
 
 app.use(bodyParser.json());
 
@@ -33,69 +19,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use("/", require("./routes"));
-
-// app.post("/send", function(req, res) {
-//   let senderName = req.body.contactFormName;
-//   let senderEmail = req.body.contactFormEmail;
-//   let messageSubject = req.body.contactFormSubjects;
-//   let messageText = req.body.contactFormMessage;
-//   let copyToSender = req.body.contactFormCopy;
-
-//   let mailOptions = {
-//     to: ["anastasios.theodosiou@gmail.com"], // Enter here the email address on which you want to send emails from your customers
-//     from: senderName,
-//     subject: messageSubject,
-//     text: messageText,
-//     replyTo: senderEmail
-//   };
-
-//   if (senderName === "") {
-//     res.status(400);
-//     res.send({
-//       message: "Bad request"
-//     });
-//     return;
-//   }
-
-//   if (senderEmail === "") {
-//     res.status(400);
-//     res.send({
-//       message: "Bad request"
-//     });
-//     return;
-//   }
-
-//   if (messageSubject === "") {
-//     res.status(400);
-//     res.send({
-//       message: "Bad request"
-//     });
-//     return;
-//   }
-
-//   if (messageText === "") {
-//     res.status(400);
-//     res.send({
-//       message: "Bad request"
-//     });
-//     return;
-//   }
-
-//   if (copyToSender) {
-//     mailOptions.to.push(senderEmail);
-//   }
-
-//   transporter.sendMail(mailOptions, function(error, response) {
-//     if (error) {
-//       console.log(error);
-//       res.end("error");
-//     } else {
-//       console.log("Message sent: ", response);
-//       res.end("sent");
-//     }
-//   });
-// });
+app.use("/mail", require("./routes"));
 
 app.listen(port, function() {
   console.log(`Server is up and running on port: ${port}`);
